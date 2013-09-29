92b77520ffb7892c454e99986197b551761f99c0
kdoba <dbkhoa@khoa.local>
2013-09-29 16:43:32 -0700

    move visit '/projects' call into before hook

--- spec/views/project/index.html.erb_spec.rb
+++ spec/views/project/index.html.erb_spec.rb
@@ -7,27 +7,25 @@ describe "projects/index.html.erb" do
       Project.create!(name: "Project"+n.to_s,
                       description: "Great projects"+n.to_s)
     end
+
+    visit '/projects'
   end
 
   it "should have a title called 'Projects'" do
-    visit '/projects'
     expect(page).to have_title('Projects')
   end
 
   it "should have a page header 'Existing Projects'" do
-    visit '/projects'
     expect(page).to have_content('Existing Projects')
   end
 
   it "should have a table with headers for name and description" do
-    visit "/projects"
     expect(page).to have_table('projects-table')
     expect(page.find('#projects-table thead tr')).to have_content("Name")
     expect(page.find('#projects-table thead tr')).to have_content("Description")
   end
 
   it "should have a content from 'Projects'" do
-    visit '/projects'
     30.times do |n|
       expect(page).to have_content('Project' + n.to_s)
     end
