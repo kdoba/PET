cc2afd1c192830a8c935a23a2b628819037d2fea
kdoba <dbkhoa@khoa.local>
2013-09-29 13:02:12 -0700

    first real rspec test case

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Project model validation tests" do
+    it "model has field name and description" do
+      should respond_to :name
+      should respond_to :description
+    end
+  end
 end
