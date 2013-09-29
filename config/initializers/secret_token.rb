9f14a066193197dd8a68b6eacd2ac54179b996a2
kdoba <dbkhoa@khoa.local>
2013-09-29 11:17:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PET::Application.config.secret_key_base = 'f5e24bf1ef681f3054e5de20e08fa36914c4645f0a9c8ecd696ee791dd862d73aff17467ef935693b2d4bf288eee4047a78ff53b5f52c89d64dfb8f04a5f1f31'
